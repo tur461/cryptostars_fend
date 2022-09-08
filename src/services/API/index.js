320d9587dc27961de825993d3bf6b45d573574a9
tur461 <tur461.er@gmail.com>
2022-09-08 12:06:54 +0530

    missed headers added to api

--- src/services/API/index.js
+++ src/services/API/index.js
@@ -17,7 +17,10 @@ function retrieveProjectVersion() {
             method: 'POST',
             body: jString({
                 projectId: MISC.PROJECT_ID,
-            })
+            }),
+            headers: {
+                'content-type': 'application/json'
+            },
         })
         .then(res => res.json())
         .then(res => r(res.data))
